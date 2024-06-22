6b0d8580a057fa993bb7561365e19caad284d762
Wasin <wasin1026@gmail.com>
2024-06-22 09:39:23 +0700

    EX6

--- ex-6.js
+++ ex-6.js
@@ -2,6 +2,8 @@ const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
   // Start coding here
+  if(!carCollection.includes(carBrand)){carCollection.push(carBrand); return "new car collection is : "+carCollection+"."}
+  else{return carBrand +" has already existed in the "+(carCollection.indexOf(carBrand)+1)+" position of car collection."}
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
